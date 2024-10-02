0c49c31a56437131e0ed21431c44e49fa6905d6f
OliverEliteMan <oliver@elite-manufacturing.com>
2024-10-02 12:51:56 -0700

    some changes & pages added as well as some sections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccordionComponent } from './accordion.component';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { Meta, Title } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet,AccordionComponent, HlmButtonDirective],
+  host: {
+    class: 'block'
+  },
+  template: `
+    <router-outlet />
+  `
 })
 export class AppComponent {
   title = 'rotomold18';
+
+  constructor(private meta: Meta, private titleService: Title) { }
+
+  updateMetaTags() {
+    this.titleService.setTitle('RotomoldMexico - Home');
+
+    // Standard Meta Tags
+    this.meta.addTag({ name: 'description', content: 'Rotomold Mexico, Family Owned with over 50 years of experience. 6 rotomolding machines with mold making capabilities. 3 distribution centers in the USA and plenty of capability to grow. Compound and pulverize in house. Request a quote or give us a call' });
+    this.meta.addTag({ name: 'keywords', content: 'Rotational Molding, Rotomold Mexico, Mold Making, Foam Insulation, Shipping, Stocking, Compression Molding, Extrusion, Rapid Prototyping, Quality Manufacturing' });
+
+    // Open Graph Meta Tags
+    this.meta.addTag({ property: 'og:title', content: 'RotomoldMexico - Home' });
+    this.meta.addTag({ property: 'og:description', content: 'Rotomold Mexico, Family Owned with over 50 years of experience. 6 rotomolding machines with mold making capabilities. 3 distribution centers in the USA and plenty of capability to grow. Compound and pulverize in house. Request a quote or give us a call' });
+    this.meta.addTag({ property: 'og:image', content: '/assets/logoHeader.png' });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./shared/components/layout/layout.component'),
+        children: [
+            {
+                path: 'home',
+                loadComponent: () => import('./pages/home/home.component')
+            },
+            {
+                path: '',
+                redirectTo: 'home',
+                pathMatch: 'full'
+            },
+            {
+                path: 'about',
+                loadComponent: () => import('./pages/about/about.component')
+            },
+            {
+                path: 'faq',
+                loadComponent: () => import('./pages/faq/faq.component')
+            },
+            {
+                path: 'services',
+                loadComponent: () => import('./pages/services/services.component')
+            },
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+    },
+];
