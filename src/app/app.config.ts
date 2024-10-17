8c24cf0ca89aff5031453477802145a424141f6b
OliverEliteMan <oliver@elite-manufacturing.com>
2024-10-17 12:30:52 -0700

    upToAzure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
+
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import {provideRouter, withInMemoryScrolling, withViewTransitions} from '@angular/router';
 import { routes } from './app.routes';
 
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes, withInMemoryScrolling({
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: "top",
+    })),
+  ]
 };
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,14 +6,15 @@ export const routes: Routes = [
         loadComponent: () => import('./shared/components/layout/layout.component'),
         children: [
             {
-                path: 'home',
-                loadComponent: () => import('./pages/home/home.component')
-            },
-            {
                 path: '',
                 redirectTo: 'home',
                 pathMatch: 'full'
             },
+            {
+                path: 'home',
+                loadComponent: () => import('./pages/home/home.component')
+            },
+            
             {
                 path: 'about',
                 loadComponent: () => import('./pages/about/about.component')
@@ -29,6 +30,30 @@ export const routes: Routes = [
             {
                 path: 'services',
                 loadComponent: () => import('./pages/services/services.component')
+            },
+            {
+                path: 'services/rotational-molding',
+                loadComponent: () => import('./pages/rotational-molding/rotational-molding.component')
+            },
+            {
+                path: 'services/shipping',
+                loadComponent: () => import('./pages/shipping/shipping.component')
+            },
+            {
+                path: 'services/mold-making',
+                loadComponent: () => import('./pages/mold-making/mold-making.component')
+            },
+            {
+                path: 'services/post-processing',
+                loadComponent: () => import('./pages/post-processing/post-processing.component')
+            },
+            {
+                path: 'services/foam-insulation',
+                loadComponent: () => import('./pages/foam-insulation/foam-insulation.component')
+            },
+            {
+                path: 'services/other-services',
+                loadComponent: () => import('./pages/other-services/other-services.component')
             },
         ]
     },
